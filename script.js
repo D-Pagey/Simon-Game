f71b686e58f1fe1abdfd3192f8cd0a0cd3f59f39
D-Pagey <dan.page91@gmail.com>
2018-04-29 02:20:22 +0100

    Adding Start button. Adding Strict functionality.

---
+++ script.js
@@ -0,0 +1,30 @@
+/**
+ * Constants
+ */
+const gameBtns = document.querySelectorAll('.btn');
+const opBtns = document.querySelectorAll('.button');
+
+let strict = false;
+
+/**
+ * Methods
+ */
+function toggleStrict() {
+    return strict = (strict ? false : true);
+}
+
+
+function enableStrict() {
+    if (strict) {
+        opBtns[2].className = 'button strict';
+        toggleStrict();
+    } else {
+        opBtns[2].className = 'button';
+        toggleStrict();
+    }
+}
+
+/**
+ * Event Listeners
+ */
+opBtns[2].addEventListener('click', enableStrict);
