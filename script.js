23df59318d869a32d1ca6ff4d07c48eec81cbded
D-Pagey <dan.page91@gmail.com>
2018-05-01 23:57:54 +0100

    Adding function to start sequence.

--- script.js
+++ script.js
@@ -5,19 +5,29 @@ const gameBtns = document.querySelectorAll('.btn');
 const opBtns = document.querySelectorAll('.button');
 
 let strict = false;
+let sequence = [];
 
 /**
  * Methods
  */
 function randomNumber() {
-    
+    return Math.floor(Math.random() * Math.floor(4));
 }
 
+function newButton() {
+    sequence.push(randomNumber());
+    return;
+}
 
 function toggleStrict() {
     return strict = (strict ? false : true);
 }
 
+function startGame() {
+    newButton();
+    opBtns[0].removeEventListener('click', startGame);
+    return;
+}
 
 function enableStrict() {
     if (strict) {
@@ -32,4 +42,9 @@ function enableStrict() {
 /**
  * Event Listeners
  */
+function enableStart() {
+    opBtns[0].addEventListener('click', startGame);
+}
+
+enableStart();
 opBtns[2].addEventListener('click', enableStrict);
