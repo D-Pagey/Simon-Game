6b54d717f22bc3d894d4011fcdde9310140721e9
D-Pagey <dan.page91@gmail.com>
2018-06-23 14:26:16 +0100

    Styling game buttons.

--- src/components/presentational/GameBtns/index.js
+++ src/components/presentational/GameBtns/index.js
@@ -4,11 +4,11 @@ import './index.css';
 
 export default function GameBtns() {
   return (
-    <div>
-      <button>Yellow</button>
-      <button>Red</button>
-      <button>Blue</button>
-      <button>Green</button>
+    <div className="game-container">
+      <button className="btn yellow"></button>
+      <button className="btn red"></button>
+      <button className="btn blue"></button>
+      <button className="btn green"></button>
     </div>
   )
 }
